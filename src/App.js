a88caebd669e8f8ae500bde59f1cade4b190a986
BadriKumar2000 <g.badrikumar2000@gmail.com>
2023-09-21 18:52:12 +0530

    added functionalities to the clock component

--- src/App.js
+++ src/App.js
@@ -3,8 +3,23 @@ import Clock from "./components/clock";
 import "./App.css";
 
 class App extends Component {
+  state = {
+    hideClock: true,
+  };
+
+  onToggleClock = () => {
+    this.setState((prevState) => ({ hideClock: !prevState.hideClock }));
+  };
+
   render() {
-    return <Clock />;
+    const { hideClock } = this.state;
+    const buttonText = hideClock ? "Show Clock" : "Hide Clock";
+    return (
+      <div className="main-container">
+        <button onClick={this.onToggleClock}>{buttonText}</button>
+        {!hideClock && <Clock />}
+      </div>
+    );
   }
 }
 
--- src/components/clock/index.js
+++ src/components/clock/index.js
@@ -1,10 +1,28 @@
 import { Component } from "react";
+import "./index.css";
 
 class Clock extends Component {
+  state = {
+    date: new Date(),
+  };
+
+  componentDidMount() {
+    this.timerId = setInterval(this.tick, 1000);
+  }
+
+  tick = () => this.setState({ date: new Date() });
+
+  componentWillUnmount() {
+    clearInterval(this.timerId);
+  }
+
   render() {
+    const { date } = this.state;
+    const time = date.toLocaleTimeString();
     return (
-      <div>
-        <h1>Clock</h1>
+      <div className="clock-container">
+        <h1 className="heading">Clock</h1>
+        <p>{time}</p>
       </div>
     );
   }
